ca427f83980a259976b828231597c7e053c95714
Chengyanzhao <451354@qq.com>
2020-01-20 10:29:27 +0800

    修改：postcss-loader使用

--- webpack.dev.js
+++ webpack.dev.js
@@ -30,16 +30,7 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'less-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => {
-                require('autoprefixer')({
-                  browsers: ['last 2 version', '>1%', 'ios 7']
-                })
-              }
-            }
-          }
+          'postcss-loader'
         ]
       },
       {
